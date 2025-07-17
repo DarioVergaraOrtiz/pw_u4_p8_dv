5c5a951b1dff5ab34ecd5108e01269d34fcdb5c5
DarioVergaraOrtiz <dcvergara@uce.edu.ec>
2025-07-16 19:40:13 -0500

    Taller 37

---
+++ src/components/Login.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="login_container">
+    <div class="login_caja">
+        <h2>Iniciar Secion</h2>
+        <input v-model="username" type="text" placeholder="Usuario" />
+        <input v-model="password" type="password" placeholder="Contraseña" />
+        <button @click="login">Ingresar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: "",
+            password: "",
+        };
+    },
+    methods: {
+        login(){
+            if(this.username === "admin" && this.password === "123") {
+                localStorage.setItem('usuario', 'admin');
+                localStorage.setItem('auth', 'true'); //Bandera que me indique al sistema que se autentico
+                //Redireccionar a la página de bienvenida
+                this.$router.push('/home');
+            } else {
+                alert("Credenciales incorrectas");
+
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.login_container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f4f6f8;
+    height: 100vh;
+    width: 100vw;
+}
+
+.login_caja {
+    background-color: #ffffff;
+    padding: 30px;
+    border-radius: 10px;
+    width: 300px;
+    text-align: center;
+}
+
+input {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 15px;
+    border: 1px solid #ccc;
+}
+
+button {
+    width: 100%;
+    padding: 10px;
+    background: #007bff;
+    color: white;
+    border: none;
+    border-radius: 6px;
+    cursor: pointer;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import EstudianteView from '../views/EstudianteView.vue'
+import LoginView from '../views/LoginView.vue'
+
+function estaAutenticado(){
+  return localStorage.getItem('auth') === 'true';
+}
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      requiereAuth: true //Protegida
+    }
   },
-  {
-    path: '/estudiante',
-    name: 'estudiante',
-    component: EstudianteView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    {
+    path: '/login',
+    name: 'login',
+    component: LoginView
   }
 ]
 
@@ -27,5 +27,18 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.requiereAuth){
+    //Si no esta autenticado
+    if(!estaAutenticado()){
+      next('/login')
+    }else{
+      next();
+    }
+  }else {
+    next();
+  }
+});
 
 export default router
